8776cf71aed84c6bfc35ea1bffce5c4671e5a78b
Zach <zachary.bretz@gmail.com>
2021-07-23 07:24:43 -0700

    Re-organized files to separate client and server

--- server/index.js
+++ server/index.js
@@ -4,9 +4,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+//  res.send('Hello World!');
 });
 
 app.listen(3000, () =>
   console.log('Example app listening on port 3000!'),
 );
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, './src/index.js'),
+  entry: path.resolve(__dirname, './client/src/index.js'),
   module: {
     rules: [
       {
@@ -15,10 +15,10 @@ module.exports = {
     extensions: ['*', '.js', '.jsx'],
   },
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, './client/dist'),
     filename: 'bundle.js',
   },
   devServer: {
-    contentBase: path.resolve(__dirname, './dist'),
+    contentBase: path.resolve(__dirname, './client/dist'),
   },
 };
