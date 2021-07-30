9f3ef2d309cc5da738c6e684efbb22828d651bc2
Zach <zachary.bretz@gmail.com>
2021-07-29 17:26:25 -0700

    Established basic routes and data queries

--- database/models/blog_commands.js
+++ database/models/blog_commands.js
@@ -12,25 +12,51 @@ let BlogModel = require('./blog')
   //   author: {userName:String}
   // })
 
-  let post = new BlogModel({
-    title:'Second Post',
-    text: 'post body',
-    author: {userName:'Zach'}
-  })
+//   let post = new BlogModel({
+//     title:'First Post',
+//     text: 'post body',
+//     author: {userName:'Zach'}
+//   })
 
-post.save()
-  .then(doc => {
-    console.log(doc)
-  })
-  .catch(err => {
-    console.log(err)
-  })
+// post.save()
+//   .then(doc => {
+//     console.log(doc)
+//   })
+//   .catch(err => {
+//     console.log(err)
+//   })
 
-
+const fetchAll = function(callback){
   BlogModel.find({})
   .then(doc => {
     console.log(doc)
+    callback(null, doc)
   })
   .catch(err => {
     console.error(err)
+    callback(err, null)
   })
+}
+
+const fetchAllbyUser = function(user, callback){
+  var author = {userName:user}
+  BlogModel.find({author: author})
+  .then(doc => {
+    console.log(doc)
+    callback(null, doc)
+  })
+  .catch(err => {
+    console.error(err)
+    callback(err, null)
+  })
+}
+
+  // BlogModel.deleteMany({blog: 'First Post'})
+  // .then(doc => {
+  //   console.log(doc)
+  // })
+  // .catch(err => {
+  //   console.error(err)
+  // })
+
+  module.exports = {fetchAll, fetchAllbyUser}
--- server/index.js
+++ server/index.js
@@ -1,13 +1,52 @@
+//wireframes: https://wireframepro.mockflow.com/editor.jsp?editor=on&bgcolor=white&perm=Create&ptitle=Blog&category=sketchui&projectid=Me041e71cad2ccc1b82cde2f445d57e991626878616100&publicid=5080642e1b82440ba58db39280e1f178#/page/D53c347159ede61ee5c22ff6d04b8fddf
 const express = require('express');
+let fetch = require('../database/models/blog_commands')
+
 
 const app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
 
 
-app.get('/', (req, res) => {
+app.get('/feed', (req, res) => {
  // res.send('Hello World!');
+ //home page
+  //post feed
+  var allPosts = fetch.fetchAll((err, data) => {
+    if(err){
+      throw(err)
+    } else{
+      res.json(data)
+      console.log(data)
+    }
+  })
 });
+
+app.get('/edit', (req, res) => {
+  // res.send('Hello World!');
+  //post view page
+ });
+
+ app.get('/:user/feed', (req, res) => {
+  // res.send('Hello World!');
+  //home page
+   //post feed
+   var user = req.params.user
+   var allPosts = fetch.fetchAllbyUser(user, (err, data) => {
+     if(err){
+       throw(err)
+     } else{
+       res.json(data)
+       console.log(data)
+     }
+   })
+ });
+
+
+ app.get('/:user/post/:id', (req, res) => {
+  // res.send('Hello World!');
+  //post create page
+ });
 
 app.listen(8000, () =>
   console.log('Example app listening on port 8000!'),
