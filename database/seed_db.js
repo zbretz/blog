afc9a37527a5830edc1b57b35b2a730e567a7fee
Zach <zachary.bretz@gmail.com>
2021-08-13 07:07:40 -0700

    Created file to insert a record into db

---
+++ database/seed_db.js
@@ -0,0 +1,22 @@
+let BlogModel = require('./models/blog')
+
+
+var userName = 'Zach'
+var title = 'Fourth Post'
+var text = 'text text texttext text texttext text texttext text texttext text texttext text texttext text texttext text texttext text texttext text texttext text texttext text texttext text texttext text text'
+var author = {userName:userName}
+
+const createPost = function(title, text, author, callback){
+  let post = new BlogModel({title,text,author})
+  post.save()
+  .then(doc => {
+    console.log(doc)
+    callback(null, doc)
+  })
+  .catch(err => {
+    console.log(err)
+    callback(err, null)
+  })
+}
+
+createPost(title,text,author
